24a060571a9bdfaa20b802f9438563eda282cbb7
FixedFusion2 <true.eggleston@ucas-edu.net>
2026-01-07 09:53:04 -0700

    fina

---
+++ individual_projetcs/financial_calculator.py
@@ -0,0 +1,17 @@
+#Financial Calculator
+#Overview
+"""Create a program that completes the following basic financial calculations:
+
+How long it will take to save for a goal based on a weekly or monthly deposit
+Compound Interest Calculator
+Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
+Sale Price Calculator (apply discounts to prices)
+Tip Calculator"""
+#Project Steps:
+"""Create a main function that will run the user interface
+Using project decomposition to figure out what other functions you need and how they interact with each other
+Implement at least one inner function within one of your main calculation functions. This inner function should handle a specific sub-task of the main function.
+Ensure that your inner function demonstrates proper scope usage, accessing variables from its outer function if necessary.
+Comment your code to explain the purpose and functionality of your inner function.
+Create your functions
+Have at least 2 people test your code to make sure it works"""
---
+++ notes/functionDecomposition.py
@@ -0,0 +1,41 @@
+#Normal Function before decorator
+def choose_letter(letter):
+    print("You chose:", letter)
+    print("This is with a decorator.")
+choose_letter("a")
+#Decorator function example
+def letter_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("Decorated function called")
+        return func(*args, **kwargs)
+    return wrapper
+
+@letter_decorator
+def choose_letter(letter):
+    print("You chose:", letter)
+
+choose_letter("a")
+
+def decorator(func):
+    def wrapper():
+        print("Before the function call")
+        func()
+        print("After the function call")
+    return wrapper
+@decorator
+def greet():
+    print("Hello!")
+
+greet()
+@decorator
+def add():
+    print(1+1)
+
+add()
+
+# Do my functions need info from other functions to work?
+# Does my function call another function?
+# Is it a helper function?
+# Is it an inner function?
+# Is it a closure function?
+# Is it a decorator function?
