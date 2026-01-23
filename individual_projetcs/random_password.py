c0c33c9a76eea17ba84368811f803f6fe2039b4b
FixedFusion2 <true.eggleston@ucas-edu.net>
2026-01-23 09:49:22 -0700

    More work on rando pass gen

--- individual_projetcs/random_password.py
+++ individual_projetcs/random_password.py
@@ -3,6 +3,8 @@
 """Create a program that allows a user to specify password requirements (length, upper/lowercase letters, numbers, and special characters) 
 then gives them 4 possible passwords they could use.
 """
+#Import Random Module
+import random
 
 #Numbers list
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -35,9 +37,61 @@ def generate_passwords():
     length = int(input("Enter the desired password length: "))
     #lowercase is set to a user input asking them if they want their password to include lowercase letters.
     lowercase = input("Include lowercase letters? (y/n): ").lower() == "y"
+    #If lowercase is yes have lowercase = True
+    if lowercase == "y":
+        lowercase = True
+    else:
+        #Else False
+        lowercase = False
     #uppercase is set to a user input asking them if they want their password to include uppercase letters.
     uppercase = input("Include uppercase letters? (y/n): ").lower() == "y"
+    #If uppercase is yes have uppercase = True
+    if uppercase == "y":
+        uppercase = True
+    else:
+        #Else False
+        uppercase = False
     #numbers is set to a user input asking them if they want their password to include numbers.
     numbers = input("Include numbers? (y/n): ").lower() == "y"
+    #If numbers is yes have numbers = True
+    if numbers == "y":
+        numbers = True
+    else:
+        #Else False
+        numbers = False
     #special characters is set to a user input asking them if they want their password to include special characters.
     special_characters = input("Include special characters? (y/n): ").lower() == "y"
+    #If special characters is yes have special_characters = True
+    if special_characters == "y":
+        special_characters = True
+    else:
+        #Else False
+        special_characters = False
+    #Create an empty list to hold all possible characters
+    all_characters = []
+    if lowercase:
+        all_characters.extend(lower_letters)
+    if uppercase:
+        all_characters.extend(upper_letters)
+    if numbers:
+        all_characters.extend(numbers)
+    if special_characters:
+        all_characters.extend(special_characters)
+
+    #Check if the user has selected at least one character type
+    if not all_characters:
+        print("Error: You must select at least one character type.")
+        return
+
+    #Generate 4 random passwords
+    passwords = []
+    for _ in range(4):
+        password = ''.join(random.choice(all_characters) for _ in range(length))
+        passwords.append(password)
+
+    #Display the generated passwords
+    print("Here are your random passwords:")
+    for pwd in passwords:
+        print(pwd)
+
+menu()
