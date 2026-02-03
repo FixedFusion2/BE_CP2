c98b1ba0fb4bcc5f8e693c37f93ae9e3a24e1b05
FixedFusion2 <true.eggleston@ucas-edu.net>
2026-02-03 09:51:43 -0700

    movie start

---
+++ individual_projetcs/movie_recommend.py
@@ -0,0 +1,39 @@
+#TE 2nd Movie Recommender
+#Import CSV
+import csv
+#Giant movie list holding the CSV File in the list with separate parts in the dictionary
+movies = []
+#Main Menu Function
+def menu():
+    #Print 1. Search/Get Recommendations
+    print("1. Search/Get Recommendations")
+    #Print 2. Print Full Movie List
+    print("2. Print Full Movie List")
+    #Print 3. Exit
+    print("3. Exit")
+    #Menu Choice is set to an input asking the user to choose 1-3.
+    menu_choice = input("Please choose an option (1-3): ")
+    #If the Menu Choice is 1, call the search_movies function.  
+    if menu_choice == "1":
+        #Call the search_movies function.
+        search_movies()
+        #Return to the main menu after searching.
+        menu()
+    #Else If the Menu Choice is 2, call the print_full_movie_list function.
+    elif menu_choice == "2":
+        #Call the print_full_movie_list function.
+        print_full_movie_list()
+        #Return to the main menu after printing the full movie list.
+        menu()
+    #Else If the Menu Choice is 3, call the exit_program function.
+    elif menu_choice == "3":
+        #Call the exit_program function.    
+        exit_program()
+        #Return to the main menu after exiting.
+        menu()
+    #Else exit program
+    else:
+        print("Invalid choice. Please select a valid option.")
+
+#Def Search Movies Function
+def search_movies():
