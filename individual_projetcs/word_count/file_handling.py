9f19796436b06d51bd88c2b20c332422d7ec7c3f
FixedFusion2 <true.eggleston@ucas-edu.net>
2026-02-19 09:49:18 -0700

    note

--- individual_projetcs/word_count/file_handling.py
+++ individual_projetcs/word_count/file_handling.py
@@ -1,5 +1,8 @@
 #Update Document Info Function
+#Notes: Save changes to file
 
 #View Document Function
+#Notes: Use r mode to read the file, but not add anything to it.
 
 #Add Content to Document Function
+#Notes: Use a+ mode to append and read to show the updated document info.
