e01e604ad0ae2e894830a717a5b102cfa785da8b
FixedFusion2 <true.eggleston@ucas-edu.net>
2026-02-20 09:27:36 -0700

    document

--- individual_projetcs/word_count/file_handling.py
+++ individual_projetcs/word_count/file_handling.py
@@ -2,23 +2,25 @@
 import csv
 
 #Update Document Info Function
-#Notes: Save changes to file 
+#Notes: Save changes to file
+def update_document():
+    document = input("Enter the exact file path for your document: ")
 
 #View Document Function
 #Notes: Use r mode to read the file, but not add anything to it.
-def view_content():
+def view_content(document):
     #Show the current file by opening the file and putting it in read mode and printing the read file
     print("Document Content:")
-    with open('individual_projetcs\word_count\document.txt', 'r') as file:
+    with open(document, 'r') as file:
         readed = file.read()
         print(f"\n{readed}\n")
 
 #Add Content to Document Function
 #Notes: Use a mode to append and r mode to read the updated document info.
-def add_content():
+def add_content(document):
     #Set content to a user input asking what you want to write.
     content = input("\nWrite the content you want on the file: ")
     #Open the file and append
-    with open('individual_projetcs\word_count\document.txt', 'a') as file:
+    with open(document, 'a') as file:
         file.write(f"\n{content}")
     print("Content added.")
