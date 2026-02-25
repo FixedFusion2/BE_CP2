3257caab4c6d85eaaf39abd44806308dd15463aa
FixedFusion2 <true.eggleston@ucas-edu.net>
2026-02-25 09:49:51 -0700

    Fractal needs to be changed

---
+++ individual_projetcs/fractal_pattern.py
@@ -0,0 +1,68 @@
+#TE 2nd Fractal Pattern Generator
+import turtle
+
+turtle.setup(width=2000, height=1000)
+
+t = turtle.Turtle()
+t.speed(10000000000000000000000000000000000000000000000000000000000000000)
+screen = turtle.Screen()
+
+t.color("blue")
+
+for i in range(3):
+    t.forward(100)
+    t.left(120)
+for i in range(3):
+    t.forward(200)
+    t.left(120)
+for i in range(3):
+    t.forward(300)
+    t.left(120)
+t.forward(100)
+t.left(60)
+t.forward(200)
+t.left(120)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(120)
+t.forward(200)
+t.right(120)
+t.forward(100)
+
+t.forward(50)
+t.left(60)
+t.forward(100)
+t.left(120)
+t.forward(50)
+t.left(60)
+t.forward(50)
+t.left(120)
+t.forward(100)
+t.right(120)
+t.forward(50)
+
+t.forward(25)
+t.left(60)
+t.forward(50)
+t.left(120)
+t.forward(25)
+t.left(60)
+t.forward(25)
+t.left(120)
+t.forward(50)
+t.right(120)
+t.forward(25)
+
+t.forward(12.5)
+t.left(60)
+t.forward(25)
+t.left(120)
+t.forward(12.5)
+t.left(60)
+t.forward(12.5)
+t.left(120)
+t.forward(25)
+t.right(120)
+t.forward(12.5)
+turtle.done()
