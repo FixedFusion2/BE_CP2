b4c4183a1bcb734f4434ff0dca33456b5c9ccf45
FixedFusion2 <true.eggleston@ucas-edu.net>
2026-03-26 09:19:20 -0600

    Start on helper

--- individual_projetcs/grade_book/src/helper.py
+++ individual_projetcs/grade_book/src/helper.py
@@ -1,1 +1,23 @@
 #TE 2nd Helper Functions for Simple Gradebook
+
+#Menu Function
+def Menu():
+    print("=====================================")
+    print("📚 SIMPLE GRADE BOOK 📚")
+    print("=====================================")
+    print("Welcome to the Class Gradebook!")
+    print("\n🎯 MAIN MENU:")
+    print("[1] Add New Student")
+    print("[2] Add Grade to Student")
+    print("[3] View Student Record")
+    print("[4] View All Students")
+    print("[5] Class Summary")
+    print("[6] Exit\n")
+
+choice = input("Enter your choice (1-6): ")
+
+#Add New Student Function
+#Add Grade to Student Function
+#View Student Record Function
+#View All Students Function
+#Class Summary Function
